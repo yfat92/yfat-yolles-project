0f174e896d44297eabeb0111ba80a883277ad11b
yfat yolles <yfat92@gmail.com>
2014-11-06 21:23:13 +0200

    basic

---
+++ src/il/ac/mta/java/Yfat_yolles_projectServlet.java
@@ -0,0 +1,13 @@
+package il.ac.mta.java;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Yfat_yolles_projectServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
