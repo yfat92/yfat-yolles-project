16e89f0f169a213c8baf1c7b8714474f14d5614e
yfat yolles <yfat92@gmail.com>
2014-11-07 14:36:11 +0200

    math calculation

--- src/il/ac/mta/java/Yfat_yolles_projectServlet.java
+++ src/il/ac/mta/java/Yfat_yolles_projectServlet.java
@@ -1,13 +1,27 @@
 package il.ac.mta.java;
 
 import java.io.IOException;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class Yfat_yolles_projectServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		
+		resp.setContentType("text/html");
+		int num1;
+		int num2;
+		int num3;
+		int res;
+		num1 = 3;
+		num2 = 4;
+		num3 = 7;
+		res =  num3*(num1+num2);		
+		String resultStr = new String("<h1>Result of " +num3+ "* (" +num2+ "+" +num1+ " ) ="  +res+"</h1>");	
+		
+		resp.getWriter().println(resultStr);
+
 	}
+
 }
