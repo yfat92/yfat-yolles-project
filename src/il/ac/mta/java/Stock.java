9990176c8fc941e70cfb8072f77e382c7927cc93
yfat yolles <yfat92@gmail.com>
2014-11-24 15:59:40 +0200

    ex01 v.03

--- src/il/ac/mta/java/Stock.java
+++ src/il/ac/mta/java/Stock.java
@@ -1,63 +1,41 @@
 package il.ac.mta.java;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.omg.CORBA.PRIVATE_MEMBER;
-
-public class Stock extends HttpServlet {
-	
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) 
-			throws IOException {
-		resp.setContentType("text/html");
-			}
-		
+public class Stock {
 	// members
-	        private float ask;
-	        private float bid;
-	        private String symbol;
-	        private java.util.Date date;
-	        // constructors
-	        
-	        public Stock(float ask, float bid, String symbol, java.util.Date date)
-	        {
-	        	this.ask=ask;
-	        	this.bid=bid;
-	        	this.symbol=symbol;
-	        	this.date=date;
-	        }
-	        // functions
-	        
-	        public void setAsk(float ask){
-	        	this.ask=ask;
-	        }
-	        public void setBid(float bid){
-	        	this.bid=bid;
-	        }
-	        public void setSymbol(String symbol){
-	        	this.symbol=symbol;
-	        }
-	        public void setDate(java.util.Date date){
-	        	this.date=date;
-	        }
-	       public float getAsk(){
-	    	   return ask;
-	       }
-	       public float getBid(){
-	    	   return bid;
-	       }
-	       public String getSymbol(){
-	    	   return symbol;
-	       }
-	       public java.util.Date getDate(){
-	    	   return date;
-	       }
-	       public String getHtmlDescription(){
-	    	   return "Symbol: " + symbol + ", ask: " + ask + ", bid: " + bid + " , date:  " + date;
-	       } 
-	     	
-	       
-	}
+    private float ask;
+    private float bid;
+    private String symbol;
+    private java.util.Date date;
+    
+    public void setAsk(float ask){
+    	this.ask=ask;
+    }
+    public void setBid(float bid){
+    	this.bid=bid;
+    }
+    public void setSymbol(String symbol){
+    	this.symbol=symbol;
+    }
+    public void setDate(java.util.Date date){
+    	this.date=date;
+    }
+   public float getAsk(){
+	   return ask;
+   }
+   public float getBid(){
+	   return bid;
+   }
+   public String getSymbol(){
+	   return symbol;
+   }
+   public java.util.Date getDate(){
+	   return date;
+   }
+      
+	public String getHtmlDescription(){
+		   return "Symbol: " + getSymbol() + ", ask: " + getAsk() + ", bid: " + getBid() + " , date:  " + getDate();
+	 }
+
+
+
+}
